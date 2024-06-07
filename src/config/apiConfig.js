587fe9e7dee26206471217a79bc5c416aaf90f78
Reinhardt Botha <botha.wr@gmail.com>
2024-06-06 22:26:31 -0700

    chore: remove apiConfig from .gitignore

---
+++ src/config/apiConfig.js
@@ -0,0 +1,4 @@
+export const API_KEY = "c40006346e5fd9a747eabd5df841e100";
+export const API_TOKEN =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNDAwMDYzNDZlNWZkOWE3NDdlYWJkNWRmODQxZTEwMCIsInN1YiI6IjY2NWJiYWMzMGI0ZjNmZjE1NjQ0ZmIwNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.DEHmnUfc7VTyQzwbkk5CMxjhn1oXHtoqv0PlLNytdeE";
+export const BASE_URL = "https://api.themoviedb.org/3";
